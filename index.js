2746776176e2c826e2fdbc0c0143f56f7eacb1b5
hahyt6644-gif <hahyt6644@gmail.com>
2025-11-25 18:39:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+
+app.get("/", async (req, res) => {
+    try {
+        const query = req.url.split("?")[1] || "";
+        const target = "http://src.is-normal.site:7782/api.php?" + query;
+
+        const response = await fetch(target);
+        const data = await response.text();
+
+        res.setHeader("Content-Type": "application/json");
+        res.send(data);
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
